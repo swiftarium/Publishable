a8d0089e938abf5b42ab7715e591c35888bb3477
jinyongp <dev.jinyongp@gmail.com>
2023-09-14 20:19:02 +0900

    feat: 기본 테스트 코드 추가

--- Tests/PublishableTests/PublishableTests.swift
+++ Tests/PublishableTests/PublishableTests.swift
@@ -1,6 +1,63 @@
 @testable import Publishable
 import XCTest
 
+private struct Model {
+    @Publishable var value: String
+}
+
+private struct A {
+    var a: String
+}
+
 final class PublishableTests: XCTestCase {
-    func testExample() throws {}
+    func testValueChangeNotifiesSubscriber() {
+        let oldValue = "OldValue"
+        let newValue = "NewValue"
+
+        var receivedOldValue: String?
+        var receivedNewValue: String?
+
+        let model = Model(value: oldValue)
+
+        model.$value.subscribe(by: self) { _, changes in
+            receivedOldValue = changes.old
+            receivedNewValue = changes.new
+        }
+
+        model.value = newValue
+
+        XCTAssertEqual(receivedOldValue, oldValue)
+        XCTAssertEqual(receivedNewValue, newValue)
+    }
+
+    func testImmediateSubscription() {
+        let oldValue = "Value"
+        let model = Model(value: oldValue)
+
+        var receivedValue: String?
+
+        model.$value.subscribe(by: self, immediate: true) { _, changes in
+            receivedValue = changes.new
+        }
+
+        XCTAssertEqual(receivedValue, oldValue)
+    }
+
+    func testUnsubscribePreventsNotification() {
+        let oldValue = "OldValue"
+        let newValue = "NewValue"
+        let model = Model(value: oldValue)
+
+        var didReceiveChange = false
+
+        model.$value.subscribe(by: self) { _, _ in
+            didReceiveChange = true
+        }
+
+        model.$value.unsubscribe(by: self)
+
+        model.value = newValue
+
+        XCTAssertFalse(didReceiveChange)
+    }
 }
