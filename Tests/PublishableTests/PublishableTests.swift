fa63cf1bf8c08cfcb310edf0bdcc5f67c344b038
jinyongp <dev.jinyongp@gmail.com>
2023-09-14 20:31:46 +0900

    feat: WeakRef 테스트 추가

--- Tests/PublishableTests/PublishableTests.swift
+++ Tests/PublishableTests/PublishableTests.swift
@@ -5,59 +5,84 @@ private struct Model {
     @Publishable var value: String
 }
 
-private struct A {
-    var a: String
+private final class Subscriber {
+    let name: String
+
+    init(name: String) {
+        self.name = name
+    }
 }
 
 final class PublishableTests: XCTestCase {
     func testValueChangeNotifiesSubscriber() {
         let oldValue = "OldValue"
         let newValue = "NewValue"
-
+        
         var receivedOldValue: String?
         var receivedNewValue: String?
-
+        
         let model = Model(value: oldValue)
-
+        
         model.$value.subscribe(by: self) { _, changes in
             receivedOldValue = changes.old
             receivedNewValue = changes.new
         }
-
+        
         model.value = newValue
-
+        
         XCTAssertEqual(receivedOldValue, oldValue)
         XCTAssertEqual(receivedNewValue, newValue)
     }
-
+    
     func testImmediateSubscription() {
         let oldValue = "Value"
         let model = Model(value: oldValue)
-
+        
         var receivedValue: String?
-
+        
         model.$value.subscribe(by: self, immediate: true) { _, changes in
             receivedValue = changes.new
         }
-
+        
         XCTAssertEqual(receivedValue, oldValue)
     }
-
+    
     func testUnsubscribePreventsNotification() {
         let oldValue = "OldValue"
         let newValue = "NewValue"
         let model = Model(value: oldValue)
-
+        
         var didReceiveChange = false
-
+        
         model.$value.subscribe(by: self) { _, _ in
             didReceiveChange = true
         }
-
+        
         model.$value.unsubscribe(by: self)
-
+        
         model.value = newValue
-
+        
         XCTAssertFalse(didReceiveChange)
+    }
+    
+    func testWeakRefRelease() {
+        let model = Model(value: "OldValue")
+        
+        var subscriber1: Subscriber? = Subscriber(name: "Subscriber 1")
+        let subscriber2: Subscriber? = Subscriber(name: "Subscriber 2")
+        var subscriber3: Subscriber? = Subscriber(name: "Subscriber 3")
+        
+        var subscribers: [String] = []
+        
+        model.$value.subscribe(by: subscriber1!) { subscribers.append($0.subscriber.name) }
+        model.$value.subscribe(by: subscriber2!) { subscribers.append($0.subscriber.name) }
+        model.$value.subscribe(by: subscriber3!) { subscribers.append($0.subscriber.name) }
+        
+        subscriber1 = nil
+        subscriber3 = nil
+        
+        model.value = "NewValue"
+        
+        XCTAssertEqual(subscribers, ["Subscriber 2"])
     }
 }
